da96c0ce01c6b72d80816c68cca43bfc3d69919b
Aaron Vargas <aaronvargas95926@gmail.com>
2018-05-18 12:36:51 -0500

    Aaron and Robbie's Bishop piece

---
+++ Bishop.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class Bishop extends Piece{
+
+	private Position position = new Position();
+	private boolean sur, sul, sdr, sdl = false; //Stop Up Right, Stop Up Left, Stop Down Right, Stop Down Left
+	
+	public Bishop(int x, int y) {
+		super(new Position(x,y));
+		position.set(x,y);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public ArrayList<Position> setPossiblePositions(ArrayList<Piece> allies, ArrayList<Piece> enemies) {
+		// TODO Auto-generated method stub
+		ArrayList<Position> returned = new ArrayList<Position>();
+		
+		
+		for (Piece ally : allies)
+		{
+			for (Piece enemy : enemies)
+			{
+				for (int x = 0; x < 8; x++) //8 is the amount of squares for each row and column, bishops only move in 1 diagonal line, so using the same value is ok
+				{
+					//If there are no allies on the tile, and position is in bounds, add it to the list. Stop checking if an enemy is found
+					//Up Right
+					if (ally.getX() != this.getX()+x && ally.getY() != this.getY()+x && super.inBounds(new Position(this.getX()+x, this.getY()+x)) && !sur)
+					{
+						returned.add(new Position(this.getX()+x, this.getY()+x));
+						if (enemy.getX() == this.getX()+x && enemy.getY() == this.getY()+x) //The location an enemy is in has already been added, stop checking after it
+							sur = true;
+					}
+					//Up Left
+					if (ally.getX() != this.getX()-x && ally.getY() != this.getY()+x && super.inBounds(new Position(this.getX()-x, this.getY()+x)) && !sul)
+					{
+						returned.add(new Position(this.getX()-x, this.getY()+x));
+						if (enemy.getX() == this.getX()+x && enemy.getY() == this.getY()+x)
+							sul = true;
+					}
+					//Down Right
+					if (ally.getX() != this.getX()+x && ally.getY() != this.getY()-x && super.inBounds(new Position(this.getX()+x, this.getY()-x)) && !sdr)
+					{
+						returned.add(new Position(this.getX()+x, this.getY()-x));
+						if (enemy.getX() == this.getX()+x && enemy.getY() == this.getY()+x)
+							sdr = true;
+					}
+					//Down Left
+					if (ally.getX() != this.getX()-x && ally.getY() != this.getY()-x && super.inBounds(new Position(this.getX()-x, this.getY()-x)) && !sdl)
+					{
+						returned.add(new Position(this.getX()-x, this.getY()-x));
+						if (enemy.getX() == this.getX()+x && enemy.getY() == this.getY()+x)
+							sdl = true;
+					}
+				}
+			}
+		}
+		return returned;
+	}
+
+}
