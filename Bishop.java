12b8dc247070ed78e826f19937dbc9519dc6a2a1
Robbie Belcher <roboslag@gmail.com>
2018-05-21 13:23:46 -0500

    bishop done

---
+++ Bishop.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Bishop extends Piece{
+
+	public Bishop(int x, int y) {
+		super(x, y);
+	}
+	
+	public Bishop(Position position) {
+		super(position);
+	}
+	
+	public Bishop reverse() {
+		return new Bishop(this);
+	}
+	
+	public ArrayList<Position> setPossiblePositions(ArrayList<Piece> allies, ArrayList<Piece> enemies) {
+		ArrayList<Position> positions = getPossiblePositions();
+		for (int x = 1; x < 8; x+=2) {
+			for(Position position : Piece.getPositions(this, x, 7)) {
+				if(allies.contains(position)) {
+					break;
+				}
+				else if(enemies.contains(position)) {
+					positions.add(position);
+					break;
+				} else {
+					positions.add(position);
+				}
+			}
+		}
+		return positions;
+	}	
+	
+	
+	
+
+}
