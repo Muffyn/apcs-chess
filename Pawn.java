9b6e8529c8e08b73efa01d2fd30989084643f2f9
afabisiak427 <39347446+afabisiak427@users.noreply.github.com>
2018-05-23 13:30:16 -0500

    En Passant Finished

--- Pawn.java
+++ Pawn.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Pawn extends Piece{
 
 	private boolean first = true;
+	private boolean justJumped = false;
 	
 	public Pawn(int x, int y) {
 		super(x, y);
@@ -16,6 +17,11 @@ public class Pawn extends Piece{
 		Pawn pawn = new Pawn(super.reverse());
 		pawn.first = this.first;
 		return pawn;
+	}
+	
+	public boolean getJustJumped()
+	{
+		return justJumped;
 	}
 	
 	public ArrayList<Position> setPossiblePositions(ArrayList<Piece> allies, ArrayList<Piece> enemies) {
@@ -36,11 +42,38 @@ public class Pawn extends Piece{
 					positions.add(position);
 			}
 		}
+		int var = canEnPassant(allies, enemies);
+		if(var != -1)
+			positions.add(enemies.get(var));
 		return positions;
 	}
 	
 	public boolean move(Position position, Player otherPlayer) {
+		int prev = this.getY();
+		boolean end;
 		first = false;
-		return super.move(position, otherPlayer);
+		end = super.move(position, otherPlayer);
+		if(prev - this.getY() == 2 || prev - this.getY() == -2)
+			justJumped = true;
+		else
+			justJumped = false;
+		return end;
+	}
+	
+	public int canEnPassant(ArrayList<Piece> allies, ArrayList<Piece> enemies) {
+		int count = 0;
+		int xCord = 0;
+		int yCord = 0;
+		for(int z = 0; z < enemies.size(); z++) {
+			if(enemies.get(z) instanceof Pawn) {
+				if(((Pawn)enemies.get(z)).getJustJumped() == true && ((xCord == enemies.get(z).getX() + 1 || xCord == enemies.get(z).getX() - 1) && (yCord == enemies.get(z).getY() + 1 || yCord == enemies.get(z).getY() - 1)))
+					count = z;
+				else
+					count = -1;
+			}
+			else
+				count = -1;
+		}
+		return count;
 	}
 }
